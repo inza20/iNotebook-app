4ceb2c8ce39aba41ccffb99b4f5e0937a5482a2a
Inshirah Zainab <inshirahzainab@gmail.com>
2023-02-08 23:51:58 +0530

    express-validator

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,25 +1,45 @@
+// auth.js will have all end points for authentication
 const express = require('express');
 const router = express.Router();
 const User = require('../models/User');
+const { body, validationResult } = require('express-validator');
+
+// router.get('/', (req, res)=> {
+//     // obj = {a: 'thios', number: 34 }
+//     // res.json(obj)                   // res.json is used to send data
+//     console.log(req.body);
+
+//     const user = User(req.body);
+//     user.save()                         // Use user.save() to store data 
+//     // res.send("hello");
+//     res.send(req.body);
+// })
+// read data via req.body and send response via res.send
+
+// ( To  create a user in db, send a user's data in 'body' of GET request )
+// Create a User using: POST "/api/auth/". Desn't require auth 
+
+router.post('/', [
+    body('name', 'Enter a valid name').isLength({min : 3}),
+    body('email', 'Enter a valid email').isEmail(),
+    body('password', 'Password must consist of atleast 5 characters').isLength({min: 5}),
+], (req, res)=> {
+    const errors = validationResult(req);
+    if(!errors.isEmpty()){
+        return res.status(400).json({errors : errors.array()});
+    }
+    User.create({
+        name: req.body.name,
+        password: req.body.password,
+        email: req.body.email,
+    }).then(user => res.json(user) );
 
 
-// Create a User using: POST "/api/auth/". Desn't require auth
-// To  create a user, send a user's data in 'body' of thunderclient GET request
-
-router.get('/', (req, res)=> {
-    // obj = {a: 'thios', number: 34 }
-    // res.json(obj)                   // res.json is used to send data
-    console.log(req.body);
-
-    const user = User(req.body);
-    user.save()                         // Use user.save() to store data 
-    // res.send("hello");
-    res.send(req.body);
+    // res.send(req.body);
 })
 
-// read data via req.body and send response via res.send
+
 
 
 module.exports = router;
 
-// auth.js will have all end points for authentication
