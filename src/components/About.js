36e26c54954333458d656c1524c68e63b805dfff
Inshirah Zainab <inshirahzainab@gmail.com>
2023-02-15 23:59:10 +0530

    Activating  nav-link

--- src/components/About.js
+++ src/components/About.js
@@ -1,18 +1,14 @@
 import React, { useContext, useEffect } from "react";
-import NoteContext from "../context/notes/NoteContext";
+// import NoteContext from "../context/notes/NoteContext";
 
 
  const About = () => {
-    const a = useContext(NoteContext);
-    useEffect(() => {
-      a.update();    
-      // eslint-disable-next-line      
-    }, [])
     
+      
     return(
         <div>
             <br/><br/> 
-            <p>This is About {a.state.name} and he is in class {a.state.class} </p>
+            <p>This is About </p>
         </div>
     )
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,44 +1,55 @@
 import PropTypes from "prop-types";
-import React from "react";
-import {Link} from "react-router-dom";
+import React, { useEffect } from "react";
+import { Link, useLocation } from "react-router-dom";
 
-export default function Navbar () {
-  
-    return (
+export default function Navbar() {
+  let location = useLocation();
+  useEffect(() => {
+    console.log(location.pathname);
+  }, [location]);
+
+  return (
     <div>
-
-<nav className="navbar fixed-top navbar-expand-lg bg-body-tertiary">
-  <div className="container-fluid">
-    <Link className="navbar-brand" to="/">iNotebook</Link>
-    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-      <span className="navbar-toggler-icon"></span>
-    </button>
-    <div className="collapse navbar-collapse" id="navbarNav">
-      <ul className="navbar-nav">
-        <li className="nav-item">
-          <Link className="nav-link active" aria-current="page" to="/">Home</Link>
-        </li>
-        {/* <li className="nav-item">
-          <Link className="nav-link" to="/general">General</Link>
-        </li> */}
-        <li className="nav-item">
-          <Link className="nav-link" to="/about">About</Link>
-        </li>
-        
-        
-      </ul>
+      <nav className="navbar fixed-top navbar-expand-lg bg-body-tertiary">
+        <div className="container-fluid">
+          <Link className="navbar-brand" to="/">
+            iNotebook
+          </Link>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarNav"
+            aria-controls="navbarNav"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarNav">
+            <ul className="navbar-nav">
+              <li className="nav-item">
+                <Link className={`nav-link ${location.pathname==="/" ? "active" : ""}`} aria-current="page" to="/">
+                  Home
+                </Link>
+              </li>
+              
+              <li className="nav-item">
+                <Link className={`nav-link ${location.pathname==="/about" ? "active" : ""}`} to="/about">
+                  About
+                </Link>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
     </div>
-  </div>
-</nav>
-        
-    </div>
-    );
-  }
-
+  );
+}
 
 Navbar.propTypes = {
-  country : PropTypes.string,
-  pageSize : PropTypes.number
-}
+  country: PropTypes.string,
+  pageSize: PropTypes.number,
+};
 
 // export default Navbar;
